7015e0467533e6c5e310dc4353f82f98813c2445
Chiaote <aaron.chiao.19@gmail.com>
2020-12-27 11:29:22 +0800

    Initial Commit

---
+++ LogAndRunLoop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogAndRunLoop
+//
+//  Created by 倪僑德 on 2020/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
